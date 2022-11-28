8c05ddc153915a167717627e9a06893de97b22b9
pawanbikram <pawanshahi49@gmail.com>
2022-11-28 14:01:40 +0545

    Added description, authors detail, fixes about, alert shifting, buttons disabled

--- src/components/About.js
+++ src/components/About.js
@@ -5,25 +5,23 @@ export default function About(props) {
     let darkMode = {
         color: "white",
         backgroundColor: "#042740",
-        border: "1px solid grey",
-        borderRadius: "5px"
+        border: "1px solid grey"
     }
 
     let lightMode = {
         color: "black",
         backgroundColor: "white",
-        border: "1px solid grey",
-        borderRadius: "5px"
+        border: "1px solid grey"
     }
 
     return (
-        <div className="container my-3" style={props.mode === "light" ? lightMode : darkMode}>
+        <div className="container my-3" style={props.mode === "light" ? { color: "black" } : { color: "white" }}>
             <h2>About Textutils</h2>
             <div className="accordion my-3" id="accordionExample">
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="headingOne">
                         <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" style={props.mode === "light" ? lightMode : darkMode}>
-                            Accordion Item #1
+                            <strong>Contact</strong>
                         </button>
                     </h2>
                     <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample" style={props.mode === "light" ? lightMode : darkMode}>
@@ -35,7 +33,7 @@ export default function About(props) {
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="headingTwo">
                         <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo" style={props.mode === "light" ? lightMode : darkMode}>
-                            Accordion Item #2
+                            <strong>Address</strong>
                         </button>
                     </h2>
                     <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample" style={props.mode === "light" ? lightMode : darkMode}>
@@ -47,7 +45,7 @@ export default function About(props) {
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="headingThree">
                         <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree" style={props.mode === "light" ? lightMode : darkMode}>
-                            Accordion Item #3
+                            <strong>Details</strong>
                         </button>
                     </h2>
                     <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample" style={props.mode === "light" ? lightMode : darkMode}>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar(props) {
         <nav className={`navbar navbar-expand-lg bg-${props.mode} navbar-${props.mode}`}>
             <div className="container-fluid">
                 <Link className="navbar-brand" to="/">{props.title}</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
